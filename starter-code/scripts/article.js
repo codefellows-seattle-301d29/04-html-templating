ed5dc2f4e56a8c7caa8a4bf48e653d1e49e68a08
warlordlizard <alexjbowers88@gmail.com>
2018-01-12 10:44:20 -0800

    handlebars  compiler built but not working yet

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,7 +13,8 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+  let source = $(#entry-template).Html();
+  let template = Handlebar.compile(source);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -22,7 +23,8 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  let context = rawData;
+  let html = template(context);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
