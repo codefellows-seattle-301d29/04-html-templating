bac108f646b23f509c1e57931a06109a8bafdf8d
AhmedAli93 <Ahmed98148@gmail.com>
2018-01-12 09:38:14 -0800

    working

--- melanie-ahmed/scripts/article.js
+++ melanie-ahmed/scripts/article.js
@@ -14,7 +14,7 @@ function Article (rawDataObj) {
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
-  let template = Handlebars.compile($('#article-template').html());
+  let template = Handlebars.compile($('#hb-template').html());
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
