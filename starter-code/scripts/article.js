07ae6f474f44421c18e865a5e19d8ba1a26acebc
Ryan Palmer <ryan.palm93@gmail.com>
2018-01-12 10:06:25 -0800

    index & .js edits.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,6 +14,9 @@ function Article (rawDataObj) {
 Article.prototype.toHtml = function() {
   // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
+var source   = document.getElementById("template").innerHTML;
+var template = Handlebars.compile(source);
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
